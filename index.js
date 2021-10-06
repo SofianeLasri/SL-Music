37bfd80cc9ccbbfd9dab07562ea53878a1912580
SofianeLasri <alasri250@gmail.com>
2021-10-06 21:22:35 +0200

    test

--- index.js
+++ index.js
@@ -225,6 +225,37 @@ const player = new Player(client, {
     leaveOnEmpty: false, // This options are optional.
 });
 
+// Init the event listener only once (at the top of your code).
+client.player
+    // Emitted when channel was empty.
+    .on('channelEmpty',  (queue) =>
+        console.log(`Everyone left the Voice Channel, queue ended.`))
+    // Emitted when a song was added to the queue.
+    .on('songAdd',  (queue, song) =>
+        console.log(`Song ${song} was added to the queue.`))
+    // Emitted when a playlist was added to the queue.
+    .on('playlistAdd',  (queue, playlist) =>
+        console.log(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
+    // Emitted when there was no more music to play.
+    .on('queueEnd',  (queue) =>
+        console.log(`The queue has ended.`))
+    // Emitted when a song changed.
+    .on('songChanged', (queue, newSong, oldSong) =>
+        console.log(`${newSong} is now playing.`))
+    // Emitted when a first song in the queue started playing.
+    .on('songFirst',  (queue, song) =>
+        console.log(`Started playing ${song}.`))
+    // Emitted when someone disconnected the bot from the channel.
+    .on('clientDisconnect', (queue) =>
+        console.log(`I was kicked from the Voice Channel, queue ended.`))
+    // Emitted when deafenOnJoin is true and the bot was undeafened
+    .on('clientUndeafen', (queue) =>
+        console.log(`I got undefeanded.`))
+    // Emitted when there was an error in runtime
+    .on('error', (error, queue) => {
+        console.log(`Error: ${error} in ${queue.guild.name}`);
+    });
+
 
 client.on("ready", async function () {
     console.log('\n'+`
@@ -260,9 +291,10 @@ client.on('interactionCreate', async interaction => {
 });
 
 // Tests
+const { RepeatMode } = require('discord-music-player');
+
 client.on('messageCreate', async (message) => {
-	let commandPrefix = "!";
-    const args = message.content.slice(commandPrefix.length).trim().split(/ +/g);
+    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
     const command = args.shift();
     let guildQueue = client.player.getQueue(message.guild.id);
 
@@ -293,15 +325,15 @@ client.on('messageCreate', async (message) => {
     }
 
     if(command === 'removeLoop') {
-        guildQueue.setRepeatMode(Player.DISABLED); // or 0 instead of Player.DISABLED
+        guildQueue.setRepeatMode(RepeatMode.DISABLED); // or 0 instead of RepeatMode.DISABLED
     }
 
     if(command === 'toggleLoop') {
-        guildQueue.setRepeatMode(Player.SONG); // or 1 instead of Player.SONG
+        guildQueue.setRepeatMode(RepeatMode.SONG); // or 1 instead of RepeatMode.SONG
     }
 
     if(command === 'toggleQueueLoop') {
-        guildQueue.setRepeatMode(Player.QUEUE); // or 2 instead of Player.QUEUE
+        guildQueue.setRepeatMode(RepeatMode.QUEUE); // or 2 instead of RepeatMode.QUEUE
     }
 
     if(command === 'setVolume') {
@@ -351,6 +383,7 @@ client.on('messageCreate', async (message) => {
         console.log(ProgressBar.prettier);
     }
 })
+
 
 
 // login to Discord with your app's token
