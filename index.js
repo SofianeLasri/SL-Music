1cc4d03c45cc53f840c07783061eb604765ab6b7
SofianeLasri <alasri250@gmail.com>
2021-10-14 13:27:57 +0200

    test

--- index.js
+++ index.js
@@ -358,9 +358,11 @@ client.on('interactionCreate', async interaction => {
 			let song = await queue.play( interaction.options.getString('titre') ).catch(async _ => {
 				if(!guildQueue)
 					queue.stop();
+					await interaction.reply("Impossible de lire la musique. :( (check la console)");
 				else {
 					await interaction.reply("Musique: "+guildQueue.nowPlaying);
 				}
+				await interaction.reply("song: "+song);
 			});
 		}
 
