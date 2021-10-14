42a213ee9d8b8dcf3be83ff196829408802f1ca8
SofianeLasri <alasri250@gmail.com>
2021-10-14 13:05:18 +0200

    ok gé kapté

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ async function initialiseDatabaseTables(){
 		}
 
 		let commandPrefix = await botSettings.findOne({where: {name: "commandPrefix" }});
-		if (commandPrefix == null){
+		if(commandPrefix == null){
 			console.log('['+'INSERT'.brightMagenta+'] Insertion de commandPrefix'.brightWhite);
 			let clientId = botSettings.create({
 				name: "commandPrefix",
